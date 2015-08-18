758b42040e502c1ef13140fe62a26d339b575a62
jeffreyan11 <jeffreyan07@gmail.com>
2015-08-18 12:10:59 -0400

    Main search captures sorted with SEE rather than MVV/LVA at low depths.
    SPRT [0, 30] at 15+0.05
    51-22-35
    LLR: 2.919

--- searchspace.cpp
+++ searchspace.cpp
@@ -65,67 +65,44 @@ void SearchSpace::generateMoves(Move hashed) {
         }
     }
 
-    // Internal iterative deepening, SEE, and MVV/LVA move ordering
+    // Internal iterative deepening and SEE move ordering
     // The scoring relies partially on the fact that our selection sort is stable
     // TODO make this cleaner, probably when captures and moves become generated separately
-    if (depth >= 3 || isPVNode) { // sort by SEE
-        // ---------------Captures----------------
-        unsigned int index = 0;
-        for (index = 0; index < legalMoves.size(); index++) {
-            Move m = legalMoves.get(index);
-            if (!isCapture(m))
-                break;
-            // If anything wins at least a rook, it should go above this anyways
-            /*if (b.getExchangeScore(color, m) > 0)
-                scores.add(ROOK_VALUE + b.getMVVLVAScore(color, legalMoves.get(index)));
-            else*/
-                scores.add(b->getSEE(color, getEndSq(m)));
-        }
-        // ---------------Non-captures----------------
-        // Score killers below even captures but above losing captures
-        for (unsigned int i = index; i < legalMoves.size(); i++) {
-            if (legalMoves.get(i) == searchParams->killers[searchParams->ply][0])
-                scores.add(0);
-            else if (legalMoves.get(i) == searchParams->killers[searchParams->ply][1])
-                scores.add(-1);
-            // Order queen promotions somewhat high
-            else if (getPromotion(legalMoves.get(i)) == QUEENS)
-                scores.add(MAX_POS_SCORE);
-            else
-                scores.add(-MATE_SCORE + searchParams->historyTable[color][b->getPieceOnSquare(color, getStartSq(legalMoves.get(i)))][getEndSq(legalMoves.get(i))]);
-        }
-
-        // IID: get a best move (hoping for a first move cutoff) if we don't
-        // have a hash move available
-        if (depth >= 5 && hashed == NULL_MOVE) {
-            int bestIndex = getBestMoveForSort(b, legalMoves, IID_DEPTHS[depth]);
-            // Mate check to prevent crashes
-            if (bestIndex == -1)
-                legalMoves.clear();
-            else
-            	scores.set(bestIndex, INFTY);
-        }
+    // ---------------Captures----------------
+    unsigned int index = 0;
+    for (index = 0; index < legalMoves.size(); index++) {
+        Move m = legalMoves.get(index);
+        if (!isCapture(m))
+            break;
+        // If anything wins at least a rook, it should go above this anyways
+        /*if (b.getExchangeScore(color, m) > 0)
+            scores.add(ROOK_VALUE + b.getMVVLVAScore(color, legalMoves.get(index)));
+        else*/
+            scores.add(b->getSEE(color, getEndSq(m)));
     }
-    else { // MVV/LVA
-        unsigned int index = 0;
-        for (index = 0; index < legalMoves.size(); index++) {
-            if (!isCapture(legalMoves.get(index)))
-                break;
-            scores.add(b->getMVVLVAScore(color, legalMoves.get(index)));
-        }
-        // For MVV/LVA, order killers above capturing a pawn with a minor piece
-        // or greater
-        for (unsigned int i = index; i < legalMoves.size(); i++) {
-            if (legalMoves.get(i) == searchParams->killers[searchParams->ply][0])
-                scores.add(PAWNS - KNIGHTS);
-            else if (legalMoves.get(i) == searchParams->killers[searchParams->ply][1])
-                scores.add(PAWNS - KNIGHTS - 1);
-            // Order queen promotions above capturing pawns and minor pieces
-            else if (getPromotion(legalMoves.get(i)) == QUEENS)
-                scores.add(8*ROOKS);
-            else
-                scores.add(-MATE_SCORE + searchParams->historyTable[color][b->getPieceOnSquare(color, getStartSq(legalMoves.get(i)))][getEndSq(legalMoves.get(i))]);
-        }
+    // ---------------Non-captures----------------
+    // Score killers below even captures but above losing captures
+    for (unsigned int i = index; i < legalMoves.size(); i++) {
+        if (legalMoves.get(i) == searchParams->killers[searchParams->ply][0])
+            scores.add(0);
+        else if (legalMoves.get(i) == searchParams->killers[searchParams->ply][1])
+            scores.add(-1);
+        // Order queen promotions somewhat high
+        else if (getPromotion(legalMoves.get(i)) == QUEENS)
+            scores.add(MAX_POS_SCORE);
+        else
+            scores.add(-MATE_SCORE + searchParams->historyTable[color][b->getPieceOnSquare(color, getStartSq(legalMoves.get(i)))][getEndSq(legalMoves.get(i))]);
+    }
+
+    // IID: get a best move (hoping for a first move cutoff) if we don't
+    // have a hash move available
+    if (depth >= 5 && hashed == NULL_MOVE) {
+        int bestIndex = getBestMoveForSort(b, legalMoves, IID_DEPTHS[depth]);
+        // Mate check to prevent crashes
+        if (bestIndex == -1)
+            legalMoves.clear();
+        else
+        	scores.set(bestIndex, INFTY);
     }
 }
 
