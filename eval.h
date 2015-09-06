e336e90e5859af6df788e3d858986bf5ec586d87
jeffreyan11 <jeffreyan07@gmail.com>
2015-09-06 16:43:22 -0400

    Added typedef and functions for SWAR eval.

--- eval.h
+++ eval.h
@@ -25,13 +25,18 @@
  * This file stores evaluation constants and encoding.
  */
 
-// Encodes 16-bit midgame and endgame evaluation scores into a single int
-#define encEval(mg, eg) ((eg << 16) | mg)
+typedef uint32_t Score;
 
-inline int decEval(int encodedValue, int egFactor) {
-    return ((encodedValue & 0xFFFF) * (EG_FACTOR_RES - egFactor)
-             + (encodedValue >> 16) * egFactor) / EG_FACTOR_RES;
+// Encodes 16-bit midgame and endgame evaluation scores into a single int
+#define encEval(mg, eg) ((Score) ((eg << 16) | mg))
+
+int decEval(Score encodedValue, int egFactor) {
+    int valueMg = (int) (encodedValue & 0xFFFF) - 0x8000;
+    int valueEg = (int) (encodedValue >> 16) - 0x8000;
+    return (valueMg * (EG_FACTOR_RES - egFactor) + valueEg * egFactor) / EG_FACTOR_RES;
 }
+
+const Score EVAL_ZERO = 0x80008000;
 
 //-------------------------Material eval constants------------------------------
 const int BISHOP_PAIR_VALUE = 55;
@@ -39,30 +44,30 @@ const int TEMPO_VALUE = 10;
 
 //------------------------Positional eval constants-----------------------------
 // King safety
-const int CASTLING_RIGHTS_VALUE[3] = {0, 23, 40};
-const int PAWN_SHIELD_VALUE = 12;
-const int P_PAWN_SHIELD_BONUS = 9;
-const int SEMIOPEN_OWN_PENALTY = 7;
-const int SEMIOPEN_OPP_PENALTY = 4;
-const int OPEN_PENALTY = 3;
+const Score CASTLING_RIGHTS_VALUE[3] = {0, 23, 40};
+const Score PAWN_SHIELD_VALUE = 12;
+const Score P_PAWN_SHIELD_BONUS = 9;
+const Score SEMIOPEN_OWN_PENALTY = 7;
+const Score SEMIOPEN_OPP_PENALTY = 4;
+const Score OPEN_PENALTY = 3;
 
 // Minor pieces
-const int BISHOP_PAWN_COLOR_PENALTY = 2;
-const int KNIGHT_PAWN_BONUS = 1;
+const Score BISHOP_PAWN_COLOR_PENALTY = 2;
+const Score KNIGHT_PAWN_BONUS = 1;
 
 // Pawn structure
 // Passed pawns
-const int PASSER_BONUS_MG[8] = {0, 10, 10, 15, 20, 30, 55, 0};
-const int PASSER_BONUS_EG[8] = {0, 30, 35, 45, 65, 95, 140, 0};
+const Score PASSER_BONUS_MG[8] = {0, 10, 10, 15, 20, 30, 55, 0};
+const Score PASSER_BONUS_EG[8] = {0, 30, 35, 45, 65, 95, 140, 0};
 // Doubled pawns
-const int DOUBLED_PENALTY_MG[7] = {0, 0, 12, 36, 72, 120, 180};
-const int DOUBLED_PENALTY_EG[7] = {0, 0, 17, 51, 102, 170, 255};
+const Score DOUBLED_PENALTY_MG[7] = {0, 0, 12, 36, 72, 120, 180};
+const Score DOUBLED_PENALTY_EG[7] = {0, 0, 17, 51, 102, 170, 255};
 // Doubled pawns are worse the less pawns you have
-const int DOUBLED_PENALTY_SCALE[9] = {0, 0, 3, 2, 1, 1, 1, 1, 1};
+const Score DOUBLED_PENALTY_SCALE[9] = {0, 0, 3, 2, 1, 1, 1, 1, 1};
 // Isolated pawns
-const int ISOLATED_PENALTY_MG = 14;
-const int ISOLATED_PENALTY_EG = 16;
+const Score ISOLATED_PENALTY_MG = 14;
+const Score ISOLATED_PENALTY_EG = 16;
 // Isolated, doubled pawns
-const int ISOLATED_DOUBLED_PENALTY = 11;
+const Score ISOLATED_DOUBLED_PENALTY = 11;
 
 #endif
