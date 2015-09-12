473c99c1371c0f4ddb3ba440d530bc60d9a1d2cc
jeffreyan11 <jeffreyan07@gmail.com>
2015-09-12 11:19:00 -0400

    Changed IID depths to a formula. No functional change.

--- searchspace.cpp
+++ searchspace.cpp
@@ -26,22 +26,6 @@ const int SCORE_EVEN_CAPTURE = (1 << 16);
 const int SCORE_LOSING_CAPTURE = 0;
 const int SCORE_QUIET_MOVE = -(1 << 30);
 
-const int IID_DEPTHS[MAX_DEPTH+1] = {0,
-     0,  0,  0,  1,  1,  1,  1,  1,  2,  2,
-     2,  3,  3,  3,  4,  4,  4,  5,  5,  6,
-     6,  6,  7,  7,  7,  8,  8,  8,  9,  9,
-     9, 10, 10, 10, 11, 11, 11, 12, 12, 12,
-    13, 13, 13, 14, 14, 14, 15, 15, 15, 16,
-    16, 16, 17, 17, 17, 18, 18, 18, 19, 19,
-    19, 20, 20, 20, 21, 21, 21, 22, 22, 22,
-    23, 23, 23, 24, 24, 24, 25, 25, 25, 26,
-    26, 26, 27, 27, 27, 28, 28, 28, 29, 29,
-    29, 30, 30, 30, 30, 30, 30, 30, 30, 30,
-    30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
-    30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
-    30, 30, 30, 30, 30, 30, 30
-};
-
 SearchSpace::SearchSpace(Board *_b, int _color, int _depth, bool _isPVNode, bool _isInCheck,
 	SearchParameters *_searchParams) {
 	b = _b;
@@ -171,7 +155,7 @@ void SearchSpace::scoreIIDMove() {
               m = nextMove());
     index = 0;*/
 
-    int iidDepth = isPVNode ? depth-2 : IID_DEPTHS[depth];
+    int iidDepth = isPVNode ? depth-2 : (depth - 3) / 3;
     int bestIndex = getBestMoveForSort(b, legalMoves, iidDepth);
     // Mate check to prevent crashes
     if (bestIndex == -1)
