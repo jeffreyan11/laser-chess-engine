bb8851ebaa7bcd77c616185fa2bde78bcf01333e
jeffreyan11 <jeffreyan07@gmail.com>
2016-05-12 02:53:33 -0700

    More aggresive late move pruning.
    1366-1354-1280 in 4000 games at 3+0.02 (+1)
    1261-1147-1592 in 4000 games at 15+0.05 (+10)
    1108-1003-1889 in 4000 games at 60+0.1 (+9)

--- search.cpp
+++ search.cpp
@@ -115,8 +115,8 @@ const int RAZOR_MARGIN[4] = {0,
 };
 
 // Move count pruning
-const unsigned int LMP_MOVE_COUNTS[7] = {0,
-    5, 8, 14, 24, 40, 64
+const unsigned int LMP_MOVE_COUNTS[8] = {0,
+    3, 5, 8, 14, 24, 40, 64
 };
 
 
@@ -835,7 +835,7 @@ int PVS(Board &b, int depth, int alpha, int beta, int threadID, SearchPV *pvLine
         // As used in Fruit/Stockfish:
         // https://chessprogramming.wikispaces.com/Futility+Pruning#MoveCountBasedPruning
         if (moveIsPrunable
-         && depth <= 6 && movesSearched > LMP_MOVE_COUNTS[depth]
+         && depth <= 7 && movesSearched > LMP_MOVE_COUNTS[depth]
          && alpha <= prevAlpha
          && m != searchParams->killers[searchParams->ply][0]
          && m != searchParams->killers[searchParams->ply][1]) {
